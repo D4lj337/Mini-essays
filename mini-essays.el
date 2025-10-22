22b2bcbf014ae2241e88bc0300df875cbb9834b1
D4lj337 <DALJEETSINGHC0@GMAIL.COM>
2025-10-22 14:52:27 +0000

    Minor fixes
    Updated author name and copyright information.

--- mini-essays.el
+++ mini-essays.el
@@ -1,15 +1,31 @@
 ;;; mini-essays.el --- Focused mini-essay writing with enforced limits -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2025
+;; Copyright (C) 2025 Daljeet singh
 
-;; Author: D4lj337
+;; Author: Daljeet singh
 ;; Version: 1.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: writing, org, productivity
 ;; URL: https://github.com/D4lj337/Mini-essays
+;; License: GPL-3.0-or-later
 
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;
 ;;; Commentary:
-
+;;
 ;; Mini-essays mode provides a focused writing environment for short-form
 ;; essays in Org-mode with enforced character/word limits, visual feedback,
 ;; and automatic templates.
